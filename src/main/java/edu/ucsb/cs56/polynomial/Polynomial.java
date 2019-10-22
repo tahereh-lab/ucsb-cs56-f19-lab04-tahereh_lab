3c9818ed006c4694d36c73400c850bacf65f2b4b
Tahereh Mehrjerdy <tahereh@umail.ucsb.edu>
2019-10-22 13:51:56 -0700

    test10

--- src/main/java/edu/ucsb/cs56/polynomial/Polynomial.java
+++ src/main/java/edu/ucsb/cs56/polynomial/Polynomial.java
@@ -459,7 +459,7 @@ public class Polynomial extends ArrayList<Integer> {
 
       */
       int  degreeOfPolynomialCoeffsLowToHigh=0;
-      for(int i=0;i<coeffsLowToHigh.lengt;i++){
+      for(int i=0;i<coeffsLowToHigh.length;i++){
 	  if(coeffsLowToHig!=0)
 	      {
 		  degreeOfPolynomialCoeffsLowToHig=i;
