b67e69a2d41ed0042efdacb2cef0a4fa3b97b55a
Tahereh Mehrjerdy <tahereh@umail.ucsb.edu>
2019-10-22 22:43:55 -0700

    i

--- src/test/java/edu/ucsb/cs56/polynomial/PolynomialTest.java
+++ src/test/java/edu/ucsb/cs56/polynomial/PolynomialTest.java
@@ -14,12 +14,12 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class PolynomialTest
 {
-  // Instead of using an @Before to set up the polynomials, I've hard coded
-  // the polynomials directly in each test.  I started settting it up
-  // with an @Before routine, but I found that it was tedious to have
-  // to keep referring back to the top of the file to see what the
-  // values of each polynomial were.  This way, you can see the entire
-  // test all on the screen at the same time.
+  Instead of using an @Before to set up the polynomials, I've hard coded
+  the polynomials directly in each test.  I started settting it up
+  with an @Before routine, but I found that it was tedious to have
+  to keep referring back to the top of the file to see what the
+  values of each polynomial were.  This way, you can see the entire
+  test all on the screen at the same time.
 
   /**
      test no arg constructor from Polynomial
@@ -27,27 +27,28 @@ public class PolynomialTest
 
    */
 
-  @Test public void testNoArgConstructor()
-  {
-    // default polynomial has degree 0, and value 0
-    Polynomial p = new Polynomial();
+     @Test public void testNoArgConstructor()
+    {
+	// default polynomial has degree 0, and value 0
+     Polynomial p = new Polynomial();
     assertEquals(0,p.getDegree());
-    assertEquals(0,(int) p.get(0));
-  }
+     assertEquals(0,(int) p.get(0));
+     }
 
   /**
-     test constructor that initializes from int array
-     @see Polynomial#Polynomial(int [] coeffs)
+   //  test constructor that initializes from int array
+   //  @see Polynomial#Polynomial(int [] coeffs)
    */
 
-  @Test public void testConstructorIntArray()
-  {
+     @Test
+     public void testConstructorIntArray()
+    {
     Polynomial p = new Polynomial(new int[] {2,1,5});
     assertEquals(2, p.getDegree());
     assertEquals(2, (int) p.get(2));
     assertEquals(1, (int) p.get(1));
     assertEquals(5, (int) p.get(0));
-  }
+    }
 
   /**
      test toString on {@code "0"}
@@ -63,66 +64,7 @@ public class PolynomialTest
     assertEquals(0,Polynomial.degreeOfPolynomialCoeffsLowToHigh(coeffsLowToHigh));
   }
 
-  /**
-     test the degreeOfPolynomialCoeffsHighToLow function on {@code new int [] {0, 0, 0, -10, -11, 0};}
-     @see Polynomial#degreeOfPolynomialCoeffsHighToLow
-   */
-  @Test
-  public void test_degreeOfPolynomialCoeffsHighToLow_2() {
-
-    int [] coeffsHighToLow = new int [] {0, 0, 0, -10, -11, 0};
-    assertEquals(2,Polynomial.degreeOfPolynomialCoeffsHighToLow(coeffsHighToLow));
-  }
-
-  /**
-     test the degreeOfPolynomialCoeffsHighToLow function on {@code new int [] {0, 0, 0, -10, 0};}
-     @see Polynomial#degreeOfPolynomialCoeffsHighToLow
-   */
-  @Test
-  public void test_degreeOfPolynomialCoeffsHighToLow_1() {
-
-    int [] coeffsHighToLow = new int [] {0, 0, 0, -10, 0};
-    assertEquals(1,Polynomial.degreeOfPolynomialCoeffsHighToLow(coeffsHighToLow));
-  }
-
-  /**
-     test the degreeOfPolynomialCoeffsHighToLow function on {@code new int [] {0, 0, 0, 10};}
-     @see Polynomial#degreeOfPolynomialCoeffsHighToLow
-   */
-  @Test
-  public void test_degreeOfPolynomialCoeffsHighToLow_0a() {
-
-    int [] coeffsHighToLow = new int [] {0, 0, 0, 10};
-    assertEquals(0,Polynomial.degreeOfPolynomialCoeffsHighToLow(coeffsHighToLow));
-  }
-
-  /**
-     test the degreeOfPolynomialCoeffsHighToLow function on all zeros
-     @see Polynomial#degreeOfPolynomialCoeffsHighToLow
-   */
-  @Test
-  public void test_degreeOfPolynomialCoeffsHighToLow_0b() {
-
-    int [] coeffsHighToLow = new int [] {0, 0, 0};
-    assertEquals(0,Polynomial.degreeOfPolynomialCoeffsHighToLow(coeffsHighToLow));
-  }
-
-  /**
-     test the degreeOfPolynomialCoeffsHighToLow function on single zero
-     @see Polynomial#degreeOfPolynomialCoeffsHighToLow
-   */
-  @Test
-  public void test_degreeOfPolynomialCoeffsHighToLow_0c() {
-
-    int [] coeffsHighToLow = new int [] {0};
-    assertEquals(0,Polynomial.degreeOfPolynomialCoeffsHighToLow(coeffsHighToLow));
-  }
-
-  /**
-     test the lowToHigh function that converts a highToLow int array of
-     coefficients into a lowToHigh int array of coefficients on a single zero
-     @see Polynomial#lowToHigh
-   */
+ 
 
 } // class PolynomialTest
 
